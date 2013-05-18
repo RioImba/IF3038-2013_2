735e099cbb29395115052f221b9166fad2cb8cc5
Sharon Loh <lohh.sharon@gmail.com>
2013-05-18 12:47:08 +0700

    memperbaiki protokol

--- progin5socket/src/progin5socket/Client2.java
+++ progin5socket/src/progin5socket/Client2.java
@@ -2,15 +2,30 @@ package progin5socket;
 
 import java.io.*;
 import java.net.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Client2 {
+    String username; 
+    
+    public Client2() {
+        username = "";
+    }
+    
+    public String getTimestamp() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyMMddHHmmss");
+        Date current = new Date();
+        String timestamp = sdf.format(current);
+        return timestamp;
+    }
+
     public static void main(String[] args) throws IOException {
+        Client2 client = new Client2();
         
         Socket echoSocket = null;
         PrintWriter out = null;
         BufferedReader in = null;
         String userInput;
-        
         String serverHostname = "127.0.0.1";
 
         if (args.length > 0) { 
@@ -34,22 +49,76 @@ public class Client2 {
 	BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
 
         System.out.println ("*** Type Message (\"logout;\" to quit)");
-        System.out.print("User input : ");
+        System.out.print(client.username + " query : ");
 	while ((userInput = stdIn.readLine()) != null) 
         {
+            //Add Timestamp
+            String[] temp_part = userInput.split(";");
+            if ( (temp_part[0].equals("check")) || ( temp_part[0].equals("uncheck")) ) {
+                userInput = userInput + ";" + client.getTimestamp();
+            }
+            
             //Send message to server
-	    out.println(userInput);
-            System.out.println("Response : " + in.readLine());
+            out.println(userInput);
+            String response = in.readLine();
+            
+            System.out.println("Response : " + response);
             
             String[] req_parts = userInput.split(";");
             
-            if (req_parts[0].equals("logout")) {
-                break;
+            if ( (req_parts[0].equals("logout")) && (req_parts.length == 2) ) {                                    
+                break; 
+            } 
+            else if ( (req_parts[0].equals("login")) && (req_parts.length == 3) ) {
+                //login;username;password
+                switch (response) {
+                    case "success":
+                        client.username = req_parts[1];
+                        System.out.println("You have been successfully logged in");
+                        break;
+                    case "failed":
+                        System.out.println("Username or password is wrong");
+                        break;
+                }
             }
-            else 
-            {
-                System.out.print("User input : ");
+            else if ( (req_parts[0].equals("check")) && (req_parts.length == 3) ) {
+                //check;id_task;timestamp                
+                //check if response = success / failed
             }
+            else if ( (req_parts[0].equals("uncheck"))  && (req_parts.length == 3) ){
+                //uncheck;id_task;timestamp                
+                //check if response = success / failed
+            }
+            else if ( (req_parts[0].equals("getlist"))  && (req_parts.length == 2) ) {
+                //getlist;username
+                
+                String[] task_list = response.split("@");
+                
+                for (int j=0; j<task_list.length; j++) {
+                    System.out.println(task_list[j]);
+                    String[] task = task_list[j].split(";");
+                    
+                    System.out.println("Task id : " + task[1]);
+                    System.out.println("Task name : " + task[2]);
+                    String[] assignee = task[3].split("#");
+                    System.out.println("Assignee : " );
+                    for (int i=0; i<assignee.length; i++) {
+                        System.out.println("  - " + assignee[i]);
+                    }
+
+                    String[] tag = task[4].split("#");
+                    System.out.println("Tag : " );
+                    for (int i=0; i<tag.length; i++) {
+                        System.out.println("  - " + tag[i]);
+                    }
+
+                    System.out.println("Category name : " + task[5]);
+                    System.out.println("Status : " + task[6]);
+                    System.out.println("");
+                }               
+            }
+            
+            System.out.print(client.username + " query : ");
 	}
 
 	out.close();
--- progin5socket/src/progin5socket/Server2.java
+++ progin5socket/src/progin5socket/Server2.java
@@ -65,12 +65,12 @@ public class Server2 extends Thread
             
             while ((inputLine = in.readLine()) != null) 
             { 
-                 System.out.println ("From client : " + inputLine); 
+                 System.out.println ("Query from " + username + " : " + inputLine); 
                  
                  String[] req_parts = inputLine.split(";");
                  
                  if ( (req_parts[0].equals("logout")) && (req_parts.length == 2) ) {                    
-                    out.println(req_parts[1] + "has been logged out.");
+                    out.println(req_parts[1] + " has been logged out.");
                     username = "";
                     break; 
                  } 
@@ -78,7 +78,10 @@ public class Server2 extends Thread
                      //login;username;password
                      if (("admin".contains(req_parts[1])) && ("admin".contains(req_parts[2]))) {
                          username = req_parts[1];
-                         out.println("Hi, " + username + "! You are logged in!");
+                         out.println("success");
+                     }
+                     else {                         
+                         out.println("failed");
                      }
                  }
                  else if ( (req_parts[0].equals("check")) && (req_parts.length == 3) ) {
@@ -102,7 +105,10 @@ public class Server2 extends Thread
                  else if ( (req_parts[0].equals("getlist"))  && (req_parts.length == 2) ) {
                      //getlist;username
                      if (!"".equals(username)) {
-                         out.println("list;id_task;task_name;status");
+                         out.println(
+                                 "list;task_id_1;task_name_1;rio#sharon#stefan;tag1#tag2#tag2#tag3;category_1;status_1@"
+                                 + "list;task_id_2;task_name_2;rio#sharon#stefan;tag1#tag2#tag2#tag3;category_2;status_2@"
+                                 + "list;task_id_3;task_name_3;rio#sharon#stefan;tag1#tag2#tag2#tag3;category_3;status_3");
                      }
                      else {
                          out.println("Please log in first");
